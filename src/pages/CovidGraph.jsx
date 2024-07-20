4825b0bd064a5333b2c3d1901ed1f017e1fdcfc3
Md Faiz <mdfaiz1201@gmail.com>
2024-07-20 15:54:18 +0530

    Added SessionStorage and minor bug fixed

--- src/pages/CovidGraph.jsx
+++ src/pages/CovidGraph.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react'
-import {fetchFromAPI} from '../Fetch/fetchFromAPI'
+import React, { useEffect, useState } from "react"
+import {fetchFromAPI} from "../Fetch/fetchFromAPI"
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -9,11 +9,11 @@ import {
   Title,
   Tooltip,
   Legend,
-} from 'chart.js';
-import { Line } from 'react-chartjs-2';
+} from "chart.js";
+import { Line } from "react-chartjs-2";
 
-import '../App.css'
-import Filter from '../components/Filter';
+import "../App.css"
+import Filter from "../components/Filter";
 
 ChartJS.register(
   CategoryScale,
@@ -30,7 +30,8 @@ export const options = {
   responsive: true,
   plugins: {
     legend: {
-      position: 'top',
+      position: "top",
+      onClick: "null"
     }
   },
   scales: {
@@ -52,19 +53,19 @@ const CovidGraph = () => {
    const [resetToggle, setResetToggle] = useState(false);
    
    useEffect(() => {
-      fetchFromAPI('statistics')
+      fetchFromAPI("statistics")
           .then(result => {
               const response = result.response;
               setCovidData(response);
           });
   
-      const storedData = sessionStorage.getItem('covidData');
+      const storedData = sessionStorage.getItem("covidData");
       if (!storedData) {
-          fetchFromAPI('statistics')
+          fetchFromAPI("statistics")
               .then(result => {
                   const response = result.response;
                   setCovidData(response);
-                  sessionStorage.setItem('covidData', JSON.stringify(response));
+                  sessionStorage.setItem("covidData", JSON.stringify(response));
               });
       } else {
           const parsedData = JSON.parse(storedData);
@@ -75,17 +76,17 @@ const CovidGraph = () => {
   useEffect(() => {
       const storedCount = sessionStorage.getItem("count");
       if (!storedCount) {
-          sessionStorage.setItem('count', count);
+          sessionStorage.setItem("count", count);
       } else {
-          count === 0 ? setCount(parseInt(storedCount) - 8) : sessionStorage.setItem('count', count);
+          count === 0 ? setCount(parseInt(storedCount) - 8) : sessionStorage.setItem("count", count);
       }
   
       const dataItem = covidData[count];
       setCurrentDate(dataItem?.day);
       if (dataItem && allData.length <= 7 && dataItem?.country !== "All") {
           setAllData([...allData, {
-              'country': dataItem?.country,
-              'cases': {
+              "country": dataItem?.country,
+              "cases": {
                   "Active": dataItem?.cases?.active || 0,
                   "Recover": dataItem?.cases?.recovered || 0,
                   "Death": dataItem?.deaths?.total || 0,
@@ -99,7 +100,7 @@ const CovidGraph = () => {
    useEffect(() => {
       if (showData){
          const newData = data.datasets.map((dataset)=>(
-            {...dataset, hidden: showData === 'All' ? false : dataset.label !== showData}
+            {...dataset, hidden: showData === "All" ? false : dataset.label !== showData}
             ))
             setNewDatasets(newData)
          }
@@ -117,29 +118,29 @@ const CovidGraph = () => {
       datasets: newDatasets || 
       [
          {
-         label: 'Death',
+         label: "Death",
          data: death,
-         borderColor: 'red',
-         backgroundColor: 'red',
+         borderColor: "red",
+         backgroundColor: "red",
          hidden: false}
          ,
          {
-         label: 'Active',
+         label: "Active",
          data: active,
-         borderColor: 'orange',
-         backgroundColor: 'orange',
+         borderColor: "orange",
+         backgroundColor: "orange",
          hidden: false},
          {
-         label: 'Recover',
+         label: "Recover",
          data: recover,
-         borderColor:'green',
-         backgroundColor:'green',
+         borderColor:"green",
+         backgroundColor:"green",
          hidden: false},
          {
-         label: 'Total Cases',
+         label: "Total Cases",
          data: totalCases,
-         borderColor: 'rgb(53, 162, 235)',
-         backgroundColor: 'rgb(53, 162, 235)',
+         borderColor: "rgb(53, 162, 235)",
+         backgroundColor: "rgb(53, 162, 235)",
          hidden: false}
       ]
    };
@@ -163,24 +164,24 @@ const CovidGraph = () => {
    }
 
    return (
-         <div className='m-auto w-3/4'>
-            <div className='flex justify-between items-center'>
-               <div className='flex flex-col'>
-                  <span className='px-3 py-1 bg-gray-200 text-left font-bold'>{currentDate || 'Loading...'}</span>
-                  <span className='flex items-center justify-center font-bold text-red-600 animate-blink'>
-                     <span className='h-2 w-2 rounded-full bg-red-600 mr-1'></span>LIVE
+         <div className="m-auto w-3/4">
+            <div className="flex justify-between items-center">
+               <div className="flex flex-col">
+                  <span className="px-3 py-1 bg-gray-200 text-left font-bold">{currentDate || "Loading..."}</span>
+                  <span className="flex items-center justify-center font-bold text-red-600 animate-blink">
+                     <span className="h-2 w-2 rounded-full bg-red-600 mr-1"></span>LIVE
                   </span>
                </div>
                <Filter setResetToggle={setResetToggle} defaultValue={resetToggle ? "All" : undefined} filterData={setShowData} />
             </div>
-            <div className='mt-2 custom-height'>
-               <Line aria-label='Graph showing COVID-19 data trends by country' options={options} data={data} />
+            <div className="mt-2 custom-height">
+               <Line aria-label="Graph showing COVID-19 data trends by country" options={options} data={data} />
             </div>
-            <div className='mt-5 flex items-center justify-center'>
+            <div className="mt-5 flex items-center justify-center">
                <button disabled = {count<=8}
                   aria-label="Previous button"
                   onClick={handlePrevious}
-                  className={`${count<=8 ? 'bg-gray-300' : 'bg-gray-500 hover:shadow-lg'} text-white rounded mr-3 text-xl border py-1 px-2`}>
+                  className={`${count<=8 ? "bg-gray-300" : "bg-gray-500 hover:shadow-lg"} text-white rounded mr-3 text-xl border py-1 px-2`}>
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                      <path strokeLinecap="round" strokeLinejoin="round" d="M18.75 19.5l-7.5-7.5 7.5-7.5m-6 15L5.25 12l7.5-7.5" />
                   </svg>
@@ -188,7 +189,7 @@ const CovidGraph = () => {
                <button disabled = {count >= 234}
                   aria-label="Next button"
                   onClick={handleNext} 
-                  className={`${count >= 234 ? 'bg-gray-300' : 'bg-gray-500 hover:shadow-lg'} ml-3 text-white rounded hover:shadow-lg text-xl border py-1 px-2`}>
+                  className={`${count >= 234 ? "bg-gray-300" : "bg-gray-500 hover:shadow-lg"} ml-3 text-white rounded hover:shadow-lg text-xl border py-1 px-2`}>
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                      <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 4.5l7.5 7.5-7.5 7.5m-6-15l7.5 7.5-7.5 7.5" />
                   </svg>
